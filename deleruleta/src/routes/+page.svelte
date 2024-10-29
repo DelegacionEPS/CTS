fd8a0287452ccdddec30f552d26ff1574e1924e4
manu <100472310@alumnos.uc3m.es>
2024-10-29 18:41:26 +0100

    tailwind instalado

--- deleruleta/src/routes/+page.svelte
+++ deleruleta/src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import '../app.css';
+</script>
+
+<h1 class="text-dele-primary text-center text-2xl">La deleruleta de la suerte</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
