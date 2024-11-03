34deafdbfd87262de309c85ab904e64c87a827f0
manu <100472310@alumnos.uc3m.es>
2024-11-03 16:06:03 +0100

    añadidos marcadores

---
+++ deleruleta/src/Marcadores.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { Card } from "flowbite-svelte";
+
+type Marcador = {
+    id: Number,
+    color: String,
+    name: String,
+    money: Number,
+    textcolor: String,
+}
+
+const num_players = 3;
+const colors = ["bg-[#fc1e1e]", "bg-[#008dea]", "bg-[#f8d42d]"];
+const textcolors = ["text-[#000000]", "text-[#000000]", "text-[#000000]"];
+
+let marcadores: Marcador[] = []; 
+
+function iniciate_players() {
+    for (let id = 0; id < num_players; id ++) {
+        marcadores.push({id: id, color: colors[id], name: "Equipo " + (id + 1).toString(), money: 0, textcolor: textcolors[id]});
+    }
+    console.log(marcadores);
+}
+iniciate_players();
+</script>
+
+<div class="absolute bottom-0 w-screen mt-4">
+    <div class="grid grid-cols-3 place-items-center text-center">
+        {#each marcadores as marcador}
+            <div class="{marcador.color} {marcador.textcolor} h-[15vh] w-[80%] rounded-xl">
+                <p class="w-full text-2xl">{marcador.name}</p>
+                <p class="w-full h-full mt-4 text-2xl">{marcador.money} puntos</p>
+            </div>
+        {/each}
+    </div>
+</div>
--- deleruleta/src/routes/+page.svelte
+++ deleruleta/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import '../app.css';
     import Panel from '../Panel.svelte';
+    import Marcadores from '../Marcadores.svelte';
 </script>
 
-<h1 class="text-dele-primary text-center text-4xl">La deleruleta de la suerte</h1>
-
 <Panel />
+<Marcadores />
