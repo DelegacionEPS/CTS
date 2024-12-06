4b6b6901ce9b3f4ee45454bdc16d00220b65203e
manu <100472310@alumnos.uc3m.es>
2024-12-06 19:22:45 +0100

    grid inicial

--- coordinadores/src/routes/+page.svelte
+++ coordinadores/src/routes/+page.svelte
@@ -4,6 +4,58 @@
 
 <!--Página principal de home-->
 
-<div class="grid grid-rows-2 place-items-center dark:bg-dark-background recompensa:bg-recompensa-background" style="min-height: 80vh;">
-    <p class="text-center text-red-500">Hola</p>
+<header class="h-[8vh] w-screen bg-[#3BC4A0]">
+    <h1 class="text-center text-2xl text-white translate-y-2/4">¡Te damos la bienvenida a nuestras comisiones!</h1>
+</header>
+
+<div class="w-screen h-screen">
+    <div class="grid grid-rows-3 w-screen">
+        <div class="grid grid-cols-3 place-items-center mt-12">
+            <div class="bg-[#ED2939] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Tecnología y Servicios
+                </p>
+            </div>
+            <div class="bg-[#FF6D2E] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Formación y Eventos
+                </p>
+            </div>
+            <div class="bg-[#FFBF1F] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Calidad y Política Universitaria
+                </p>
+            </div>
+        </div>
+        <div class="grid grid-cols-3 place-items-center mt-12">
+            <div class="bg-[#F341A1] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Representación Externa
+                </p>
+            </div>
+            <div class=""></div>
+            <div class="bg-[#3BC4A0] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Orlas
+                </p>
+            </div>
+        </div>
+        <div class="grid grid-cols-3 place-items-center mt-12">
+            <div class="bg-[#001389] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Coordinación
+                </p>
+            </div>
+            <div class="bg-[#5599DD] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Igualdad y Diversidad
+                </p>
+            </div>
+            <div class="bg-[#09725F] p-4 h-[20vh] w-[25vw] rounded-xl">
+                <p class="text-white text-center translate-y-2/4">
+                    Comisión de Comunicación y Diseño
+                </p>
+            </div>
+        </div>
+    </div>
 </div>
